020dca6347ab750601e3c456797ecf122cc71c84
Anne Caroline <carolaragao94@gmail.com>
2022-07-06 20:16:49 -0300

    Colisões

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,19 @@ const jump = () => {
         mario.classList.remove("jump-mario");
     }, 500);
 }
+
+const loopGame = setInterval(() => {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "./assets/images/mario-game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "45px";
+
+    clearInterval(loopGame)
+})
+
+document.addEventListener("keydown", jump);
