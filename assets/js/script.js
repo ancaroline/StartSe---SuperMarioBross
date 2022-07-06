37a592a8306ba4bc4c14c95412e5b633f9b2a462
Anne Caroline <carolaragao94@gmail.com>
2022-07-06 19:55:20 -0300

    html e css desenvolvidos

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const mario = document.querySelector(".super-mario");
+const pipe = document.querySelector(".pipe-game");
+
+const jump = () => {
+    mario.classList.add("jump-mario");
+
+    setTimeout(() => {
+        mario.classList.remove("jump-mario");
+    }, 500);
+}
